817cf599d0d02bf0cce29a84e88b00e06a8f2f68
thomas-neville <thomasneville01@gmail.com>
2021-09-13 14:00:06 -0700

    added input and buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import ToDoList from './ToDoList';
 
 function App() {
   return (
-    <ToDoList />
+    <>
+      <ToDoList />
+      <input type="text"></input>
+      <button>Add ToDo</button>
+      <button>Clear Completed ToDoList</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
