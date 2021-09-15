383cb6a5030c4aa62d965f56d18008f226f0805d
thomas-neville <thomasneville01@gmail.com>
2021-09-15 11:19:12 -0700

    added click handler and setTodos function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import ToDoList from './ToDoList';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false }])
+  const [todos, setTodos] = useState([])
+
+  // add function for adding todo item, passing in event, useRef hook to access things in html like the input
+  const todoNameRef = useRef() // ref attribute for todo input
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    
+    // Setting the Todos, take previous todos, spread operator on array
+    setTodos(prevTodos => {
+      return [...prevTodos, { id:1, name: name, complete: false}]
+    })
+    
+    // clear the input field by resetting the value to null
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
       <ToDoList todos={ todos } />
-      <input type="text"></input>
-      <button>Add ToDo</button>
+      <input ref={todoNameRef} type="text"></input>
+      <button onClick={handleAddTodo}>Add ToDo</button>
       <button>Clear Completed ToDoList</button>
       <div>0 remaining</div>
     </>
--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Todo({ todo }) {
   return (
     <div>
-      {todo.name}
+      <label>
+        <input type="checkbox" checked={todo.complete}></input>
+        {todo.name}
+      </label>
     </div>
   )
 }
